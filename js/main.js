15332b114d11ea89c18f4e2e47a467e1ed44a345
this_valery <zubkovalera96@gmail.com>
2023-12-20 14:26:02 +0500

    Главная страница

--- js/main.js
+++ js/main.js
@@ -1227,20 +1227,20 @@
     /*
     // .block-finder
     */
-    $(function () {
-        $('.block-finder__form-control--select select').on('change', function() {
-            const item = $(this).closest('.block-finder__form-control--select');
+    // $(function () {
+    //     $('.block-finder__form-control--select select').on('change', function() {
+    //         const item = $(this).closest('.block-finder__form-control--select');
 
-            if ($(this).val() !== 'none') {
-                item.find('~ .block-finder__form-control--select:eq(0) select').prop('disabled', false).val('none');
-                item.find('~ .block-finder__form-control--select:gt(0) select').prop('disabled', true).val('none');
-            } else {
-                item.find('~ .block-finder__form-control--select select').prop('disabled', true).val('none');
-            }
+    //         if ($(this).val() !== 'none') {
+    //             item.find('~ .block-finder__form-control--select:eq(0) select').prop('disabled', false).val('none');
+    //             item.find('~ .block-finder__form-control--select:gt(0) select').prop('disabled', true).val('none');
+    //         } else {
+    //             item.find('~ .block-finder__form-control--select select').prop('disabled', true).val('none');
+    //         }
 
-            item.find('~ .block-finder__form-control--select select').trigger('change.select2');
-        });
-    });
+    //         item.find('~ .block-finder__form-control--select select').trigger('change.select2');
+    //     });
+    // });
 
     /*
     // .block-header
@@ -1273,9 +1273,9 @@
     /*
     // select2
     */
-    $(function () {
-        $('.form-control-select2, .block-finder__form-control--select select').select2({width: ''});
-    });
+    // $(function () {
+    //     $('.form-control-select2, .block-finder__form-control--select select').select2({width: ''});
+    // });
 
     /*
     // .vehicle-form
---
+++ js/zv.js
@@ -0,0 +1,95 @@
+console.log('[zv.js]')
+
+let jsonFetch = fetch('js/data.json')
+
+let fToJson = jsonFetch.then(function (fetchRes) {
+    console.log('[zv.js] fetchRes', fetchRes)
+    return fetchRes.json()
+})
+
+fToJson.then(function (resJson) {
+    console.log('[zv.js] resJson', resJson)
+    setDataPage(resJson)
+})
+
+function setDataPage(data) {
+    console.log('[setDataPage]', data)
+
+    activateAnalog(data) // Заполнение программ для аналогов
+    activateFeaturesProg(data) // Заполнение возможностей программ
+    activateSelect2(data) // Заполнение формы поиска
+}
+
+function activateAnalog(data) {
+    // Заполнение программ для аналогов
+    let owlCarusel = $('#owl-carousel-1')
+
+    for (let prog of data.program) {
+        let aap = addAnalogProg(prog)
+        owlCarusel.trigger('add.owl.carousel', aap)
+        // place_insertion.append(aap)
+    }
+    owlCarusel.trigger('remove.owl.carousel', [0]) // Удалить первый шаблонный элемент карусели
+    owlCarusel.trigger('refresh.owl.carousel') // Обновить корусель
+}
+
+function addAnalogProg(data) {
+    let analog_prog_template = document.querySelector('.zv-analog-prog')
+    let new_ap = analog_prog_template.cloneNode(true)
+
+    new_ap.querySelector('.zv-p-src-analogs')
+    new_ap.querySelector('.zv-p-src-product').innerHTML = data.name
+    
+    // place_insertion.append(new_ap)
+    // console.log('[addAnalogProg]', data, new_ap)
+    return new_ap
+}
+
+function activateFeaturesProg(data) {
+    let owlCarusel2 = $('#owl-carousel-2')
+
+    for (let prog of data.skill) {
+        let aap = addFeaturesProg(prog)
+        owlCarusel2.trigger('add.owl.carousel', aap)
+        // place_insertion.append(aap)
+    }
+    owlCarusel2.trigger('remove.owl.carousel', [0]) // Удалить первый шаблонный элемент карусели
+    owlCarusel2.trigger('refresh.owl.carousel') // Обновить корусель
+}
+
+function addFeaturesProg(data) {
+    let analog_prog_template = document.querySelector('.zv-features-prog')
+    let new_ap = analog_prog_template.cloneNode(true)
+
+    new_ap.querySelector('.zv-f-src-id').innerHTML = data.id
+    new_ap.querySelector('.zv-f-src-name').innerHTML = data.name
+
+    return new_ap
+}
+
+function activateSelect2(data) {
+    console.log('activateSelect2', data)
+
+    let features = document.querySelector('.zv-select-features')
+    for (let c of data.skill) {
+        let new_c = document.createElement('option')
+        new_c.setAttribute('value', c.id)
+        new_c.innerHTML = c.name
+        features.append(new_c)
+    }
+    
+    let country = document.querySelector('.zv-select-country')
+    for (let c of data.country) {
+        let new_c = document.createElement('option')
+        new_c.setAttribute('value', c.id)
+        new_c.innerHTML = c.name
+        country.append(new_c)
+    }
+    
+    /*
+    // select2
+    */
+    $(function () {
+        $('.form-control-select2, .block-finder__form-control--select select').select2({width: ''});
+    });
+}
